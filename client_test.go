9e633447d05840d93f2ab5d39a5ce954236b4cd3
Donn Lee <git@pluza.com>
2016-05-02 12:28:28 -0700

    Added test for redis write over Go/golang channel. Next: Need to check response of write commands.

--- client_test.go
+++ client_test.go
@@ -3,6 +3,8 @@ package chredis
 import(
   "crypto/rand"
   "encoding/hex"
+  "fmt"
+  "sync"
   "testing"
   "github.com/stretchr/testify/assert"
   "github.com/stretchr/testify/require"
@@ -26,10 +28,13 @@ func TestCmd(t *testing.T) {
   w, err := dial_and_create(t)
   require.Nil(t, err)
 
+  defer w.Close()
+
   // Test bogus command.
   r := w.Cmd("non-existant-command")
   assert.Equal(
-    t, r.Err.Error(), 
+    t,
+    r.Err.Error(),
     "ERR unknown command 'non-existant-command'")
   assert.NotNil(t, r.Err)
 
@@ -39,4 +44,26 @@ func TestCmd(t *testing.T) {
   assert.Nil(t, r.Err)
   r = w.Cmd("DEL", key)
   assert.Nil(t, r.Err)
+
+  // Test write via channel.
+  ch := make(chan Rhash)
+  var wg sync.WaitGroup
+  wg.Add(1)
+  go w.ReceiveFromChannelAndWriteToServer(ch, &wg)
+  randstr := randStr()
+  for i := 0; i < 5; i++ {
+    key = fmt.Sprintf("%s:%d", randstr, i)
+    rhash := Rhash{key, map[string]string{"f1":"v1", "f2":"v2"}}
+    ch <- rhash
+  }
+  close(ch)
+  fmt.Println("Waiting for goroutine to finish...")
+  wg.Wait()
+  fmt.Println("Goroutine is done. End of test func.")
+  // Clean-up.
+  for i := 0; i < 5; i++ {
+    key = fmt.Sprintf("%s:%d", randstr, i)
+    r = w.Cmd("DEL", key)
+    assert.Nil(t, r.Err)
+  }
 }
