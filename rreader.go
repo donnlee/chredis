e9d98f5e81e67b7a8ad2b32b51949002a6e9f4ee
Donn Lee <git@pluza.com>
2016-05-03 10:50:43 -0700

    Now Rreeader returns a Rhash struct instead of a plain map.

--- rreader.go
+++ rreader.go
@@ -7,13 +7,26 @@ import(
 
 type Rreader interface {
   Cmd(cmd string, args ...interface{}) *redis.Resp
-  ReadRhash(key string) (map[string]string, error)
+  ReadRhash(key string) (Rhash, error)
   Close() error
 }
 
 // Read a redis hash from the redis server.
-func (rc *Rclient) ReadRhash(key string) (map[string]string, error) {
+func (rc *Rclient) readHash(key string) (map[string]string, error) {
   m, err := rc.Cmd("HGETALL", key).Map()
-  fmt.Println(m)
   return m, err
 }
+
+// Given a key, read a hash, returning a Rhash struct.
+func (rc *Rclient) ReadRhash(key string) (Rhash, error) {
+  m, err := rc.readHash(key)
+  if err != nil {
+    // Just propagate err upstream for handling.
+    return Rhash{}, err
+  }
+  fmt.Println("")
+  return Rhash{key, m}, nil
+}
+
+// TODO: Goroutine that retrieves a stream of keys, reads hashes, and shoves Rhash'es into ch --> 2nd ch just prints the hashes (as a demo implementation).
+
