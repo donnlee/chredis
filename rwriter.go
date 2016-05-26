4f9bd208bafeb82e8a1fa7883e0b48746aed110e
Donn Lee <git@pluza.com>
2016-05-26 10:30:15 -0700

    rwriter: Conform to golint

--- rwriter.go
+++ rwriter.go
@@ -6,7 +6,7 @@ import (
 	"github.com/mediocregopher/radix.v2/redis"
 )
 
-// Methods a redis writer can do.
+// Rwriter defines the methods a redis writer can do.
 type Rwriter interface {
 	Cmd(cmd string, args ...interface{}) *redis.Resp
 	WriteRhash(h Rhash) *redis.Resp
@@ -14,6 +14,7 @@ type Rwriter interface {
 	ReceiveFromChannelAndWriteToServer(ch chan Rhash, wg *sync.WaitGroup)
 }
 
+// Rclient is a wrapper for redis.Client
 type Rclient struct {
 	Client redis.Client
 }
@@ -32,9 +33,7 @@ func explodeMapStringString(m map[string]string) []interface{} {
 	return arr
 }
 
-/*
- * Connect to redis server and return a new redis client.
- */
+// Dial connects to the redis server (it's a wrapper).
 func Dial(protocol, addr string) (*Rclient, error) {
 	c, err := redis.Dial(protocol, addr)
 	if err != nil {
@@ -45,19 +44,19 @@ func Dial(protocol, addr string) (*Rclient, error) {
 	return &rc, err
 }
 
-// Execute a redis command.
+// Cmd is a wrapper of redis.Cmd().
 func (rc *Rclient) Cmd(cmd string, args ...interface{}) *redis.Resp {
 	return rc.Client.Cmd(cmd, args)
 }
 
-// Write a redis hash to the redis server.
+// WriteRhash writes a redis hash to the redis server.
 func (rc *Rclient) WriteRhash(h Rhash) *redis.Resp {
 	// redis.Cmd() accepts a slice of interface{}, so can't use []string.
-	rhash_as_slice := []interface{}{h.Key}
-	fv_map_as_slice := explodeMapStringString(h.Fv_map)
-	rhash_as_slice = append(rhash_as_slice, fv_map_as_slice...)
+	rhashAsSlice := []interface{}{h.Key}
+	fvMapAsSlice := explodeMapStringString(h.Fv_map)
+	rhashAsSlice = append(rhashAsSlice, fvMapAsSlice...)
 	// Send slice to variadic fn with triple-dots ("myslice...")
-	return rc.Cmd("HMSET", rhash_as_slice...)
+	return rc.Cmd("HMSET", rhashAsSlice...)
 }
 
 // Close the TCP connection.
@@ -65,24 +64,23 @@ func (rc *Rclient) Close() error {
 	return rc.Client.Close()
 }
 
-/*
- * Receive Rhash struct's from channel & write them to redis server.
- * Execute this as a goroutine and shove Rhash's at it. Close the channel
- * when you are done and this func will complete/exit.
- */
+// ReceiveFromChannelAndWriteToServer receives Rhash struct's from channel &
+// writes them to redis server.
+// Execute this as a goroutine and shove Rhash's at it. Close the channel
+// when you are done and this func will complete/exit.
 func (rc *Rclient) ReceiveFromChannelAndWriteToServer(
 	ch chan Rhash, wg *sync.WaitGroup) {
 
 	// Start the goroutine for resp checking.
-	resp_ch := make(chan *redis.Resp)
-	go rc.receiveRespAndCheckForNil(resp_ch, wg)
+	respCh := make(chan *redis.Resp)
+	go rc.receiveRespAndCheckForNil(respCh, wg)
 
 	for h := range ch {
 		// Send resp to the next goroutine for checking success of the write.
-		resp_ch <- rc.WriteRhash(h)
+		respCh <- rc.WriteRhash(h)
 	}
 	// We end up here when ch is closed.
-	close(resp_ch)
+	close(respCh)
 }
 
 /*
@@ -93,10 +91,10 @@ func (rc *Rclient) ReceiveFromChannelAndWriteToServer(
  * Panic can be recover()'d by callers of this func.
  */
 func (rc *Rclient) receiveRespAndCheckForNil(
-	resp_ch chan *redis.Resp, wg *sync.WaitGroup) {
+	respCh chan *redis.Resp, wg *sync.WaitGroup) {
 
 	defer wg.Done()
-	for resp := range resp_ch {
+	for resp := range respCh {
 		if resp.Err != nil {
 			// TODO: Do something better than panic.
 			panic(resp.Err)
